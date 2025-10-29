7de04c8588877c43cbdbdf5fe5af5606c4adc4df
mirac firat <miracfirat.17@gmail.com>
2025-10-29 09:25:44 +0900

    sample1

---
+++ Sample1/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("삼각형의 밑변을 입력하세요: ");
+        double base = input.nextFloat();
+
+        System.out.print("삼각형의 높이를 입력하세요 ");
+        double height = input.nextDouble();
+
+        double area = 0.5 * base * height;
+        System.out.printf("삼각형의 넓이는 %.2f 입니다.%n", area);
+    }
+}
